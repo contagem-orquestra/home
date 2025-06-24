3216a1258dab5ebaf2413e90716c3fef7945b94e
Orquestra <contagemrp@gmail.com>
2025-06-24 19:12:54 -0300

    Adicionado configuração de PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Orquestra",
+        short_name: "Orquestra",
+        description: "Orquestra",
+        theme_color: "#e47c34",
+        icons: [
+          {
+            src: "icon-192.png",
+            sizes: "128x128",
+            type: "image/png",
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   base: "https://contagem-orquestra.github.io/home/",
 });
