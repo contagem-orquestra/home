76646ae70a379586924a54bfae2dafa4fe34badf
Orquestra <contagemrp@gmail.com>
2025-06-24 19:10:21 -0300

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://contagem-orquestra.github.io/home/sw.js', { scope: 'https://contagem-orquestra.github.io/home/' })})}
